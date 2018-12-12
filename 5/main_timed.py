ea2dd7443748f1c75caa717b6409eafebbbe1741
legent <leandrogentili@gmail.com>
2018-12-12 18:25:40 +0100

    done hw5

--- 5/main_timed.py
+++ 5/main_timed.py
@@ -1,18 +1,26 @@
 import numpy as np
-import time, sys
+import time
+import sys
 from functools import reduce
 
-# faster method to calculate divisors of n
-def get_divisors(n):    
+def timeit(method):
+    def timed(*args, **kw):
+        ts = time.perf_counter()
+        result = method(*args, **kw)
+        te = time.perf_counter()
+        print('%r  %2.10f s' % (method.__name__, (te - ts)))
+        return result
+    return timed
+
+def factors(n):    
     return set(reduce(list.__add__, ([i, n//i] for i in range(1, int(n**0.5) + 1) if n % i == 0)))
 
-# main function
+def get_divisors(i):
+	return [x for x in range(1, i + 1) if i % x == 0]
+
+@timeit
 def get_trasformation(input_):
-
-	# shallow copy
 	output_ = input_[::]
-
-	# empty vector
 	transition_ = np.zeros(len(input_), dtype=int)
 
 	# looping backwards
@@ -24,16 +32,13 @@ def get_trasformation(input_):
 		# add to transition vector
 		transition_[i - 1] = 1
 
-		# switch each bulb that's a divisor of i
-		for d in get_divisors(i):
-
-			# switch the state
+		# switch each bulb
+		for d in factors(i):
 			output_[d - 1] = 1 if output_[d - 1] == 0 else 0
 
-	# print out through list expansion
 	print(*transition_)
 
-# main
+
 if __name__ == '__main__':
 	
 	# process input
